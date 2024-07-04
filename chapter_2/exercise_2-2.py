df5e4ebfe0e2ca7dc640468a46cdb17bc85d43f9
ClaudioRMalvino <claudiormal@gmail.com>
2024-07-04 18:21:15 -0400

    Updated exercise_2-2.py

--- chapter_2/exercise_2-2.py
+++ chapter_2/exercise_2-2.py
@@ -5,7 +5,7 @@ Exercise 2.2 Altitude of a satellite
 
 Textbook: Computational Physics by Mark Newman
 """
-def altitude(T):
+def altitude_of_sat(T):
     """
     Function calculates the orbital height of a satellite when given an orbital period T.
     Returns the height in meters.
@@ -16,26 +16,24 @@ def altitude(T):
     h = ((G * M * (T**2))/(4 * (pi**2)))**(1/3) - R
     print(f"Required altitude: {round(h, 0)} m")
 
-def altitude_of_sat():
+def main():
     """
     Function holds the logical operations for selectiong the desired metric of orbital period T.
     It then calls function altitude(T) to calculate the orbital height of the satellite utilizing
     the input. 
     """
-    q = True
-
-    while q:
+    while True:
         T_input = input("If desired orbital period is in minutes input 'min'\ntype 'q' to quit\nInput orbital period (in hours): ")
-
+        if T_input == 'q':
+            break
         if T_input == "min":
             T_input = int(input("Input orbital period (in minutes): "))
             T_sec = 60 * T_input
-            altitude(T_sec)
-        elif T_input == "q":
-            q = False
+            altitude_of_sat(T_sec)
         else:
             T_sec = 3600 * float(T_input)
-            altitude(T_sec)
+            altitude_of_sat(T_sec)
 
-altitude_of_sat()
+if __name__ == "__main__":
+    main()
 
