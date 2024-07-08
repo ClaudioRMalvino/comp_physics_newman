52a017ec1eefc7bc5addff0f83e63d27aa180cd1
Claudio Malvino <claudiormal@gmail.com>
2024-07-08 17:14:51 -0400

    Uploading completed exercises 2.4 and 2.5

---
+++ chapter_2/exercise_2-4.py
@@ -0,0 +1,74 @@
+from math import sqrt
+
+"""
+Exercise 2.4: Calculating travel time from two different reference frames at relativistic speeds.
+"""
+
+c = 2.0E+8  # Speed of light in m/s
+
+
+def earth_ref_frame(x, v):
+    """
+    Function that calculates the time it takes to travel distance x at a
+    fraction of the speed of light v*c from Earth reference point.
+
+    int x: distance in light years
+    float v: a value (0,1] 
+
+    returns travel time in light years
+    """
+    x_meters = x * 9.460730E+15
+    beta = v*c
+    return x_meters/beta
+
+
+def ship_ref_frame(x, v):
+    """
+    Function that calculates proper time from the reference frame of the passenger on the
+    ship traversing a distance x at a velocity a fraction of the speed of light c
+    """
+    gamma = 1 / sqrt(1 - v**2)
+    return earth_ref_frame(x, v)/gamma
+
+
+def main():
+    """
+    Functions asks inputs for x (distance in light years) and v (a number (0,1]) to 
+    to calculate the time it takes for a spaceship to travel a distance x at velocity v*c
+    for Earth reference frame and passenger reference frame. 
+    """
+
+    while True:
+        print("\nCalculating travel time from Earth reference frame and passenger reference frame.")
+        print("Input 'q' to quit.")
+
+        x_input = input("Input distance in lightyears: ")
+        if x_input == 'q':
+            break
+
+        v_input = input(
+            "Input velocity as a fraction of speed of light (number from (0,1]):")
+        if v_input == 'q':
+            break
+        try:
+            x = float(x_input)
+            v = float(v_input)
+
+            if v <= 0 or v >= 1:
+                raise ValueError("Velocity must be between (0,1]")
+
+            earth_frame = earth_ref_frame(x, v) / 31556952
+            ship_frame = ship_ref_frame(x, v) / 31556952
+
+            print(f"\nTravel time from Earth reference frame: {
+                  round(earth_frame, 2)}")
+            print(f"Travel time from ship reference frame: {
+                  round(ship_frame, 2)}\n")
+
+        except ValueError as e:
+            print(f"Error:{e}. Please try again.")
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ chapter_2/exercise_2-5.py
@@ -0,0 +1,96 @@
+from math import sqrt
+
+"""
+Exercise 2.5: Quantum Potential Step
+
+Textbook: Computational Physics by Mark Newman
+"""
+
+# Global constants
+m = 0.5109989461  # Mass of electron in eV
+h_bar = 6.5821196  # Reduced Planck constant in eV*s
+
+
+def k1(E):
+    """
+    Function which calculates initial wave vector k1.
+
+    float E: Initial energy of the electron
+
+    returns float
+    """
+    return sqrt(2*m*E / h_bar)
+
+
+def k2(E, V):
+    """
+    Function which calculates reflected wave vector k2.
+
+    float E: Initial energy of the electron
+    float V: Potential step
+
+    returns float
+    """
+    return sqrt(2*m*(E - V) / h_bar)
+
+
+def T(k1, k2):
+    """
+    Function calculates the probability of transmission over the potential step given k1 and k2.
+
+    float k1: initial wavevector form function k1(E, V)
+    float k2: wavevector which passes across the potential step k2(E, V)
+
+    return float: probability as a percentage of transmission
+    """
+
+    return (4 * k1 * k2) / (k1 + k2)**2
+
+
+def R(k1, k2):
+    """
+    Function calculates the probability of reflecting back towards intial source
+    given k1 and k2.
+
+    float k1: initial wavevector form function k1(E, V)
+    float k2: wavevector which passes across the potential step k2(E, V)
+
+    return float: probability as a percentage of reflection
+    """
+    return ((k1 - k2) / (k1 + k2))**2
+
+
+def main():
+
+    print("Calculating transmission and reflection probabilities.")
+    print("Input 'q' to quit")
+
+    while True:
+
+        E_input = input("Input energy of electron in eV: ")
+        if E_input == 'q':
+            break
+
+        V_input = input("Input energy of potential step in eV: ")
+        if V_input == 'q':
+            break
+
+        E = float(E_input)
+        V = float(V_input)
+
+        if E <= V:
+            k = -k1(E)
+            print(f"wavevector: {k}, Probability of Reflection: 100%")
+
+        print(f"\nGiven an electron of mass {m} eV, initial energy {
+
+              E} eV and potential step {V} eV:\n")
+        print(f"Probability of reflection:{
+              round(R(k1=k1(E), k2=k2(E, V))*100, 1)}% || wavevector : {round(-k1(E), 3)}")
+        print(f"Probability of transmission:{
+              round(T(k1=k1(E), k2=k2(E, V))*100, 1)}% || wavevector: {round(k2(E, V), 3)}")
+
+
+if __name__ == '__main__':
+
+    main()
