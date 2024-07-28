12f0dd30f8d3493fdcfbec54b6472eb29dc0e562
Claudio Malvino <claudiormal@gmail.com>
2024-07-28 19:30:34 -0400

    Updating exercise 2.4 to have a more accurate value for the speed of light.

--- chapter_2/exercise_2-4.py
+++ chapter_2/exercise_2-4.py
@@ -4,7 +4,7 @@ from math import sqrt
 Exercise 2.4: Calculating travel time from two different reference frames at relativistic speeds.
 """
 
-c = 2.0E+8  # Speed of light in m/s
+c = 299792458.0  # Speed of light in m/s
 
 
 def earth_ref_frame(x, v):
@@ -13,13 +13,13 @@ def earth_ref_frame(x, v):
     fraction of the speed of light v*c from Earth reference point.
 
     int x: distance in light years
-    float v: a value (0,1] 
+    float v: a value (0,1]
 
     returns travel time in light years
     """
-    x_meters = x * 9.460730E+15
-    beta = v*c
-    return x_meters/beta
+    x_meters = x * 9.460730e15
+    beta = v * c
+    return x_meters / beta
 
 
 def ship_ref_frame(x, v):
@@ -28,46 +28,53 @@ def ship_ref_frame(x, v):
     ship traversing a distance x at a velocity a fraction of the speed of light c
     """
     gamma = 1 / sqrt(1 - v**2)
-    return earth_ref_frame(x, v)/gamma
+    return earth_ref_frame(x, v) / gamma
+
 
 if __name__ == "__main__":
 
     def main():
         """
-        Functions asks inputs for x (distance in light years) and v (a number (0,1]) to 
+        Functions asks inputs for x (distance in light years) and v (a number (0,1]) to
         to calculate the time it takes for a spaceship to travel a distance x at velocity v*c
-        for Earth reference frame and passenger reference frame. 
+        for Earth reference frame and passenger reference frame.
         """
-    
+
         while True:
-            print("\nCalculating travel time from Earth reference frame and passenger reference frame.")
+            print(
+                "\nCalculating travel time from Earth reference frame and passenger reference frame."
+            )
             print("Input 'q' to quit.")
-    
+
             x_input = input("Input distance in lightyears: ")
-            if x_input == 'q':
+            if x_input == "q":
                 break
-    
+
             v_input = input(
-                "Input velocity as a fraction of speed of light (number from (0,1]):")
-            if v_input == 'q':
+                "Input velocity as a fraction of speed of light (number from (0,1]):"
+            )
+            if v_input == "q":
                 break
             try:
                 x = float(x_input)
                 v = float(v_input)
-    
+
                 if v <= 0 or v >= 1:
                     raise ValueError("Velocity must be between (0,1]")
-    
+
                 earth_frame = earth_ref_frame(x, v) / 31556952
                 ship_frame = ship_ref_frame(x, v) / 31556952
-    
-                print(f"\nTravel time from Earth reference frame: {
-                      round(earth_frame, 2)}")
-                print(f"Travel time from ship reference frame: {
-                      round(ship_frame, 2)}\n")
-    
+
+                print(
+                    f"\nTravel time from Earth reference frame: {
+                      round(earth_frame, 2)}"
+                )
+                print(
+                    f"Travel time from ship reference frame: {
+                      round(ship_frame, 2)}\n"
+                )
+
             except ValueError as e:
                 print(f"Error:{e}. Please try again.")
-
 
     main()
