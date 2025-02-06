10fccbf2530d580eca0d7288e696b9ca0550a480
Claudio Ricardo Malvino <142508361+ClaudioRMalvino@users.noreply.github.com>
2025-02-06 16:28:58 -0500

    Update exercise_2-3.py

--- chapter_2/exercise_2-3.py
+++ chapter_2/exercise_2-3.py
@@ -17,26 +17,26 @@ def cart_to_polar(x, y):
           y}) -> Polar Coordinates: ({round(r, 2)}, {round(theta * (180 / pi), 2)})")
 
 
+def main():
+    """
+    Function performs the process of taking the inputs for the Cartesian coordinates (x,y).
+    Then utilizes the cart_to_polar(x,y) function to calculate the provided inputs.
+    """
+
+    while True:
+        inputx = input("Converting Cartesian to Polar\nInput 'q' to quit \
+                        \nInput x coordinate: ")
+        if inputx == 'q':
+            break
+        inputy = input("\nInput y coordinate: ")
+        if inputy == 'q':
+            break
+
+        x = float(inputx)
+        y = float(inputy)
+        cart_to_polar(x, y)
+
 if __name__ == "__main__":
-
-    def main():
-        """
-        Function performs the process of taking the inputs for the Cartesian coordinates (x,y).
-        Then utilizes the cart_to_polar(x,y) function to calculate the provided inputs.
-        """
-
-        while True:
-            inputx = input("Converting Cartesian to Polar\nInput 'q' to quit \
-                            \nInput x coordinate: ")
-            if inputx == 'q':
-                break
-            inputy = input("\nInput y coordinate: ")
-            if inputy == 'q':
-                break
-
-            x = float(inputx)
-            y = float(inputy)
-            cart_to_polar(x, y)
 
 
     main()
