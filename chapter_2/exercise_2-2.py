9b55a9b3fe9a5297ea9dd454cdc39662783a9b93
Claudio Ricardo Malvino <142508361+ClaudioRMalvino@users.noreply.github.com>
2025-02-06 16:28:33 -0500

    Update exercise_2-2.py

--- chapter_2/exercise_2-2.py
+++ chapter_2/exercise_2-2.py
@@ -22,32 +22,31 @@ def altitude_of_sat(T):
 
     print(f"Required altitude: {round(h, 0)} m")
 
+def main():
+    """
+    Function holds the logical operations for selecting the
+    desired metric of orbital period T.
 
+    It then calls function altitude(T) to calculate the
+    orbital height of the satellite utilizing the input.
+    """
+    while True:
+        T_input = input(
+            "If desired orbital period is in minutes input 'min' \n \
+            type 'q' to quit \n \
+            Input orbital period (in hours): "
+        )
+
+        if T_input == "q":
+            break
+        if T_input == "min":
+            T_input = int(input("Input orbital period (in minutes): "))
+            T_sec = 60 * T_input
+            altitude_of_sat(T_sec)
+        else:
+            T_sec = 3600 * float(T_input)
+            altitude_of_sat(T_sec)
+            
 if __name__ == "__main__":
-
-    def main():
-        """
-        Function holds the logical operations for selecting the
-        desired metric of orbital period T.
-
-        It then calls function altitude(T) to calculate the
-        orbital height of the satellite utilizing the input.
-        """
-        while True:
-            T_input = input(
-                "If desired orbital period is in minutes input 'min' \n \
-                type 'q' to quit \n \
-                Input orbital period (in hours): "
-            )
-
-            if T_input == "q":
-                break
-            if T_input == "min":
-                T_input = int(input("Input orbital period (in minutes): "))
-                T_sec = 60 * T_input
-                altitude_of_sat(T_sec)
-            else:
-                T_sec = 3600 * float(T_input)
-                altitude_of_sat(T_sec)
 
     main()
