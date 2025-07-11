31735b346af3f2d640a3b21c4747d9dc3ca91c52
ClaudioRMalvino <claudiormal@gmail.com>
2025-07-10 22:06:20 -0500

    Made some corrections on review of the file. Had some typos and the logic for r_min abd r_max was incorrect when raising the value error.

--- chapter_3/exercise_3_6.py
+++ chapter_3/exercise_3_6.py
@@ -22,7 +22,7 @@ def logistic_map(r, x0, iterations):
     x = np.zeros(iterations)
     x[0] = x0
     for i in range(1, iterations):
-        x[i] = r * x[i-1] * (1 - x[i-1])
+        x[i] = r * x[i - 1] * (1 - x[i - 1])
     return x
 
 
@@ -43,7 +43,6 @@ def feigenbaum_plot(r_min, r_max, dr, x0, iterations):
     for r in r_values:
         x = logistic_map(r, x0, iterations)
         x_values.extend([(r, xi) for xi in x])
-
     return zip(*x_values)
 
 
@@ -59,19 +58,19 @@ def main():
     while True:
 
         r_min_input = input("Input r_min (range: [-2, 4]): ")
-        if r_min_input == 'q':
+        if r_min_input == "q":
             break
         r_max_input = input("Input r_max (range: [-2, 4]): ")
-        if r_max_input == 'q':
+        if r_max_input == "q":
             break
         dr_input = input("Input increment (dr must be > 0): ")
-        if dr_input == 'q':
+        if dr_input == "q":
             break
         x0_input = input("Input initial x value (x0 must be >0): ")
-        if x0_input == 'q':
+        if x0_input == "q":
             break
         iter_input = input("Input number of iterations (must be > 0): ")
-        if iter_input == 'q':
+        if iter_input == "q":
             break
         try:
             r_min = float(r_min_input)
@@ -81,7 +80,7 @@ def main():
             iter = int(iter_input)
 
             if r_min > r_max:
-                raise ValueError("r_min must be greater than r_max")
+                raise ValueError("r_min must be less than r_max")
             if r_min < 2 and r_min > 4:
                 raise ValueError("r_min must be within [2, 4]")
             if r_max < 2 and r_max > 4:
@@ -98,10 +97,10 @@ def main():
 
             # Plots the Feigenbaum Diagram
             plt.figure(figsize=(12, 8))
-            plt.plot(r, x, ',k', alpha=0.4, markersize=0.1)
-            plt.title('Feigenbaum Diagram (Logistic Map)')
-            plt.xlabel('r')
-            plt.ylabel('x')
+            plt.plot(r, x, ",k", alpha=0.4, markersize=0.1)
+            plt.title("Feigenbaum Diagram (Logistic Map)")
+            plt.xlabel("r")
+            plt.ylabel("x")
             plt.ylim(0, 1)
             plt.show()
 
